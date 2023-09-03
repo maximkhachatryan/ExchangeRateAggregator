11e879a792bcde0597227f336a8387817a82b5e5
Maxim <maxim.khachatryan.2013@gmail.com>
2023-09-03 15:13:14 +0400

    Implement Web parsers abstraction.

---
+++ ExchangeRateAggregator.ApplicationServices/WebParsers/ParserBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateAggregator.ApplicationServices.WebParsers
+{
+    internal class ParserBase
+    {
+        private readonly string _source;
+
+        public ParserBase(string source)
+        {
+            this._source = source;
+        }
+    }
+}
---
+++ ExchangeRateAggregator.ApplicationServices/WebParsers/ParsersByApi/ParserByApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateAggregator.ApplicationServices.WebParsers.ParsersByApi
+{
+    internal class ParserByApi : ParserBase
+    {
+        public ParserByApi(string source) : base(source)
+        {
+        }
+    }
+}
