43f3c2593d5b96c04fa01a1199ceb795e15ad137
Maxim <maxim.khachatryan.2013@gmail.com>
2023-09-05 23:19:06 +0400

    Clean up

--- ExchangeRateAggregator.Api/Program.cs
+++ ExchangeRateAggregator.Api/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddExchangeRateAggregatorSwagger();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 #endregion
 
 #region App Scope
